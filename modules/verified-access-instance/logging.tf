8d9454fd68232354e21eed0e874f28c54d33e9e5
lakshmi-jainapally-hod <varalakshmi.jainapally@digital.homeoffice.gov.uk>
2026-06-03 14:41:24 +0100

    fix: va s3 access logs https

--- modules/verified-access-instance/logging.tf
+++ modules/verified-access-instance/logging.tf
@@ -148,6 +148,40 @@ resource "aws_s3_bucket_public_access_block" "va_access_logs" {
   restrict_public_buckets = true
 }
 
+data "aws_iam_policy_document" "va_access_logs_https_only" {
+  count = var.use_centralized_log_bucket ? 0 : 1
+
+  statement {
+    sid    = "DenyInsecureTransport"
+    effect = "Deny"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = ["s3:*"]
+
+    resources = [
+      aws_s3_bucket.va_access_logs[0].arn,
+      "${aws_s3_bucket.va_access_logs[0].arn}/*"
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "va_access_logs_https_only" {
+  count = var.use_centralized_log_bucket ? 0 : 1
+
+  bucket = aws_s3_bucket.va_access_logs[0].id
+  policy = data.aws_iam_policy_document.va_access_logs_https_only[0].json
+}
+
 resource "aws_s3_bucket_versioning" "va_access_logs" {
   count = var.use_centralized_log_bucket ? 0 : 1
 
@@ -228,6 +262,40 @@ resource "aws_s3_bucket_public_access_block" "va_logs_block_public" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+data "aws_iam_policy_document" "va_log_bucket_https_only" {
+  count = var.use_centralized_log_bucket ? 0 : 1
+
+  statement {
+    sid    = "DenyInsecureTransport"
+    effect = "Deny"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = ["s3:*"]
+
+    resources = [
+      aws_s3_bucket.va_log_bucket[0].arn,
+      "${aws_s3_bucket.va_log_bucket[0].arn}/*"
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "va_log_bucket_https_only" {
+  count = var.use_centralized_log_bucket ? 0 : 1
+
+  bucket = aws_s3_bucket.va_log_bucket[0].id
+  policy = data.aws_iam_policy_document.va_log_bucket_https_only[0].json
 }
 
 resource "aws_s3_bucket_versioning" "va_logs_versioning" {
